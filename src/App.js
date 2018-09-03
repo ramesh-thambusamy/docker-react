44c86bae339f055efd9ed0509a8ead3422566353
Ramesh.Thambusamy <ramesh.thambusamy1@gmail.com>
2018-09-03 17:52:29 -0500

    branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <h1>Hi there !!!.......</h1>
+          <h1>Hi there branch !!! </h1>
         </p>
       </div>
     );
